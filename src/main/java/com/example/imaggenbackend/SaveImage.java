ab8ac2e61870d045725abb18e36c3be34f2c69b5
Vishal N <61023404+vishalnandagopal@users.noreply.github.com>
2023-03-07 00:49:29 +0530

    backup

--- src/main/java/com/example/imaggenbackend/SaveImage.java
+++ src/main/java/com/example/imaggenbackend/SaveImage.java
@@ -3,7 +3,6 @@ package com.example.imaggenbackend;
 import java.io.InputStream;
 import java.io.IOException;
 import java.util.Scanner;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,43 +10,46 @@ import java.nio.file.Paths;
 public class SaveImage {
     public static void save(String remoteImgURL) throws IOException {
 
-        InputStream rawbytes = HTTPCaller.fetch(new URL(remoteImgURL), "", "", "", "");
-        // Create a Path object for the target file in the local directory
-        Path path = Paths.get("images", "image.png");
+        InputStream rawBytesOfImage = HTTPCaller.fetch(remoteImgURL, "", "", "", "");
+        try {
+            // Create a Path object for the target file in the local directory
+            Path path = Paths.get("images", "image.png");
 
-        // Create the necessary directories if they don't exist
-        Files.createDirectories(path.getParent());
+            // Create the necessary directories if they don't exist
+            Files.createDirectories(path.getParent());
 
-        // Create the target file if it doesn't exist
-        if (Files.exists(path)) {
-            Scanner sc = new Scanner(System.in);
-            try {
-                System.out
-                        .print("A file already exists with the given filename. Do you want to overwrite it? [Y/n]? :");
-                String a = sc.nextLine();
+            // Create the target file if it doesn't exist
+            if (Files.exists(path)) {
+                Scanner sc = new Scanner(System.in);
+                try {
+                    System.out
+                            .print("A file already exists with the given filename. Do you want to overwrite it? [Y/n]? :");
+                    String a = sc.nextLine();
 
-                if (a == "Y" || a == "" || a == "y") {
-                    System.out.println(path);
-                    boolean deleted = Files.deleteIfExists(path);
-                    if (deleted) {
-                        System.out.println("File deleted successfully");
+                    if (a == "Y" || a == "" || a == "y") {
+                        System.out.println(path);
+                        rawBytesOfImage.close();
+                        boolean deleted = Files.deleteIfExists(path);
+                        if (deleted) {
+                            System.out.println("File deleted successfully");
+                        }
                     }
+                    Files.createFile(path);
+                } finally {
+                    sc.close();
                 }
-                Files.createFile(path);
-            } finally {
-                sc.close();
             }
+
+            // Use the Files.copy() method to copy the input stream to the target file
+            Files.copy(rawBytesOfImage, path);
+        } finally { // Close the input stream
+            rawBytesOfImage.close();
         }
-
-        // Use the Files.copy() method to copy the input stream to the target file
-        Files.copy(rawbytes, path);
-
-        // Close the input stream
-        rawbytes.close();
-        System.out.println("done");
+        System.out.println("Saved image to ");
     }
 
     public static void main(String args[]) throws IOException {
         SaveImage.save("https://idjh.files.wordpress.com/2011/12/google-ics.jpg");
     }
+    
 }
--- src/test/java/com/example/imaggenbackend/CheckHTTPResponse.java
+++ src/test/java/com/example/imaggenbackend/CheckHTTPResponse.java
@@ -1,20 +1,17 @@
 package com.example.imaggenbackend;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
 //
 import static org.junit.Assert.assertEquals;
 //@RunWith(JUnitPlatform.class)
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CheckHTTPResponse {
 
